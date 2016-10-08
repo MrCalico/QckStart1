fc8204d90231aabebf9a7e5c50dc722739b642bc
John <John@ApexMicro.com>
2016-10-08 16:06:13 -0700

    just another commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>My First Anqular 2.0 zApp</h1>'
 })
 export class AppComponent { }
